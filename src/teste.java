e5094a8dad23db7d97943066e86c4d16bca0de5a
Reinaldo Junior <reifsjunior@gmail.com>
2024-04-26 13:01:57 -0300

    Update cin Controle de fluxo

---
+++ src/teste.java
@@ -0,0 +1,9 @@
+public class teste {
+    public static void main(String[] args) {
+        boolean condicao = false;
+
+        while(condicao){
+            System.out.println("executou ... ");
+        }
+    }
+}
